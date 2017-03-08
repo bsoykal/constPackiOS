a71eb65114769dcf7cc347e4ec6d201ef809d711
bartukusculuoglu <bartu@Bartus-MacBook-Pro.local>
2017-03-08 20:11:12 +0200

    Ana ekran : hatali sifre girildiginde uyari vermesi

--- ConstPack/ViewController.m
+++ ConstPack/ViewController.m
@@ -34,46 +34,61 @@ RestManager *restManager;
 - (IBAction)buttonTapped:(UIButton *)sender
 {
     LoginRequest* loginRequest = [[LoginRequest alloc] init];
+    LoginResponse* loginresponse1 = [[LoginResponse alloc] init];
     //loginRequest.eposta = @"emrahgenc@outlook.com";
     //loginRequest.password = @"123";
     NSString *pulledUserName = userName.text;
     NSString *pulledPasswd   = passwd.text;
+    Boolean loginCheck       = true;
     
     loginRequest.eposta   = pulledUserName;
     loginRequest.password = pulledPasswd;
     
     if (pulledUserName.length == 0 || pulledPasswd.length == 0) {
-    
+        loginCheck = false;
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hata"
-                                                        message:@"Lütfen boşlukları doldurun."
+                                                        message:@"Lütfen kullanıcı adı ve sifrenizi kontrol edip tekrar deneyin.."
                                                        delegate:nil
                                               cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
         [alert show];
-        
+        [self.view.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+    }else{
+        //[self.view.window. dismissViewControllerAnimated:YES completion:nil];
     }
-    /*[restManager doLoginRequest:loginRequest
-     
-                     onResponse: ^(LoginResponse *loginresponse) {
-                         
-                         [restManager doGetProjectsRequest : [NSString stringWithFormat:@"%d",loginresponse.data.kullaniciId]
-                                                 onResponse: ^(ProjectsResponse* projectsResponse){
-                                                     NSLog(@"Success :: project size %lu",(unsigned long)[projectsResponse.data count]);
-                                                 }
-                                                onError: ^(Error *error){
-                                                        NSLog(@"Error occured :: projects request");
-                                                    }];
-
-                         
-                         
-                         
-                     }onError: ^(Error *error){
-                         
-                         NSLog(@"Error occured :: loginrequest");
-
-                     }];
     
-    */
+    
+    if (loginCheck == true) {
+        [restManager doLoginRequest:loginRequest
+         
+                         onResponse: ^(LoginResponse *loginresponse) {
+                             
+                             [restManager doGetProjectsRequest : [NSString stringWithFormat:@"%d",loginresponse.data.kullaniciId]
+                                                     onResponse: ^(ProjectsResponse* projectsResponse){
+                                                         NSLog(@"Success :: project size %lu",(unsigned long)[projectsResponse.data count]);
+                                                     }
+                                                        onError: ^(Error *error){
+                                                            NSLog(@"Error occured :: projects request");
+                                                        }];
+                             
+                             
+                            
+                             
+                         }onError: ^(Error *error){
+                             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hata"
+                                                      message:@"Lütfen kullanıcı adı ve sifrenizi kontrol edip tekrar deneyin."
+                                                      delegate:nil
+                                                      cancelButtonTitle:@"OK"
+                                                      otherButtonTitles:nil];
+                             [alert show];
+                             [self.view.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+                             NSLog(@"Error occured :: loginrequest");
+                             
+                         }];
+
+    }
+    
+    
 
  
 }
